76e3253f490475e319ad99cd39a9c67a18ada429
sulyman002 <oyedelesulaiman@gmail.com>
2025-06-13 20:03:59 +0100

    on the javascript

--- script.js
+++ script.js
@@ -8,16 +8,16 @@
 
 
 
-    // const buttons = document.querySelectorAll('button');
+// const buttons = document.querySelectorAll('button');
 
-    // buttons.forEach(button => {
+// buttons.forEach(button => {
 
-    //     button.addEventListener('click', () => {
-    //         const ids = button.getAttribute('data-id');
-    //         const targetDiv = document.querySelector('.cd-' + ids);
-    //         targetDiv.style.display = 'none';
-    //     });
-    // });
+//     button.addEventListener('click', () => {
+//         const ids = button.getAttribute('data-id');
+//         const targetDiv = document.querySelector('.cd-' + ids);
+//         targetDiv.style.display = 'none';
+//     });
+// });
 
 
 // const brightness = document.querySelector('.header>div:last-child>img');
@@ -42,10 +42,47 @@
 
 //Question can in put a one section of function in a wrap so that other function wont affect it?
 
-const body = document.querySelector('#body');
-const toggle = document.querySelector('#icon');
+// const body = document.querySelector('#body');
+// const toggle = document.querySelector('#icon');
 
 
-toggle.addEventListener('click', function () {
-    body.classList.toggle('bright');
+// toggle.addEventListener('click', function () {
+//     body.classList.toggle('bright');
+// });
+
+const cardarray = [...document.querySelectorAll('.card')];
+
+const cardContainer = document.querySelector('.cards_container');
+
+const all = document.querySelector('.all');
+const active = document.querySelector('.active');
+const inactive = document.querySelector('.inactive');
+
+const inputs = document.querySelectorAll('input[type="checkbox"]');
+const checkedInputs = Array.from(inputs).filter(input => input.checked);
+
+const checkedCards = checkedInputs.map(input => input.closest('.card'));
+
+
+active.addEventListener('click', () => {
+    
+  checkedCards.forEach(card => {
+    card.style.display = 'grid';
+  });
+});
+
+
+// const checkedCards = cardarray.filter(card => {
+//     const input = card.querySelector('input[type="checkbox"]');
+//     return input && input.checked;
+// });
+
+// active.addEventListener('click', function() {
+//     checkedCards.style.display = 'grid';
+// })
+
+
+
+all.addEventListener('click', function () {
+    cardContainer.style.display = 'grid';
 });
